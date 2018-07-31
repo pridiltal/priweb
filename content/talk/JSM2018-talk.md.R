d42c24503873a134d34b868b3aa3e4ff9c2a5aa3
Priyanga Talagala <priyangatalagala@Priyangas-MacBook-Air.local>
2018-07-31 04:08:04 -0700

    updated JSM 2018 talk

--- content/talk/JSM2018-talk.md.R
+++ content/talk/JSM2018-talk.md.R
@@ -1,32 +1,32 @@
 +++
-  date = "2018-07-31"
-  title = "Outlier Detection in Non-Stationary Data Streams"
-  abstract = ""
-  abstract_short = ""
-  event = "2018 Joint Statistical Meetings - American Statistical Association"
-  event_url = "http://ww2.amstat.org/meetings/jsm/2018/"
-  location = "Vancouver Convention Centre, Vancouver, British Columbia, Canada"
+date = "2018-07-31"
+title = "Outlier Detection in Non-Stationary Data Streams"
+abstract = ""
+abstract_short = ""
+event = "2018 Joint Statistical Meetings - American Statistical Association"
+event_url = "http://ww2.amstat.org/meetings/jsm/2018/"
+location = "Vancouver Convention Centre, Vancouver, British Columbia, Canada"
   
   
-  selected = false
-  math = true
+selected = false
+math = true
   
-  url_pdf = "https://robjhyndman.com/papers/oddstream.pdf"
+url_pdf = "https://robjhyndman.com/papers/oddstream.pdf"
   
-  url_slides = "talks/USER2018/JSM-2018.html"
-  url_video = ""
-  
-  
-  
-  # Optional featured image (relative to `static/img/` folder).
-  [header]
-  #image = "headers/talks_ISF2017.jpg"
-  #caption = "37th International Symposium on Forecasting Cairns, Australia."
+url_slides = "talks/USER2018/JSM-2018.html"
+url_video = ""
   
   
-  #Embed your slides or video here using [shortcodes](https://gcushen.github.io/hugo-academic-demo/post/writing-markdown-latex/). Further details can easily be added using *Markdown* and $\rm \LaTeX$ math code. 
   
-  +++
+# Optional featured image (relative to `static/img/` folder).
+#[header]
+#image = "headers/talks_ISF2017.jpg"
+#caption = "37th International Symposium on Forecasting Cairns, Australia."
+  
+  
+#Embed your slides or video here using [shortcodes](https://gcushen.github.io/hugo-academic-demo/post/writing-markdown-latex/). Further details can easily be added using *Markdown* and $\rm \LaTeX$ math code. 
+  
++++
     
     
     
